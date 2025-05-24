a94c13f7751e41d2c501f8964067737bb7c11c7e
pedromorais03 <pedromorais3012@gmail.com>
2025-05-24 16:37:40 -0300

    Checking nmap installation

--- host-enum.sh
+++ host-enum.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Verifying nmap installation
+if ! command -v nmap &> /dev/null; then
+    echo "ERROR: Nmap not installed"
+    exit 1
+fi
+
 # Enum host
 if [ -z "$1" ]; then
     echo "Use $0 <network ip>"
@@ -9,5 +15,9 @@ fi
 
 NETWORK=$1
 
+# Checking CIDR notation
+
 echo "Scanning network $NETWORK. Searching for active hosts"
 nmap -sn $NETWORK | grep "Nmap scan report for" | awk '{print $5}' > active_hosts.txt
+
+# Checking file and if has content
