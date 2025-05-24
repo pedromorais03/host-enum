ecc2924cb5eda5d20fc4f5404deb03922125aeca
pedromorais03 <pedromorais3012@gmail.com>
2025-05-24 16:51:11 -0300

    Added some checks and added .gitignore

--- host-enum.sh
+++ host-enum.sh
@@ -2,7 +2,7 @@
 
 # Verifying nmap installation
 if ! command -v nmap &> /dev/null; then
-    echo "ERROR: Nmap not installed"
+    echo "ERROR: Nmap not installed."
     exit 1
 fi
 
@@ -15,9 +15,18 @@ fi
 
 NETWORK=$1
 
-# Checking CIDR notation
+# Checking CIDR notation using regex
+if [[ ! "$NETWORK" =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+/[0-9]+$ ]]; then
+    echo "ERROR: Please use CIDR notation. Example: 0.0.0.0/0"
+    exit 1
+fi
 
 echo "Scanning network $NETWORK. Searching for active hosts"
 nmap -sn $NETWORK | grep "Nmap scan report for" | awk '{print $5}' > active_hosts.txt
 
 # Checking file and if has content
+if [ -s active_hosts.txt ]; then
+    echo "Nmap has detect active hosts. Please check'em in active_hosts.txt"
+else
+    echo "No file found!"
+fi
